bc33d14129c14984d2cc531a970a9548b13f67c0
Claudia Lim <climweiching@gmail.com>
2023-09-15 12:28:47 +0100

    first draft

--- index.php
+++ index.php
@@ -1,45 +1,121 @@
 <?php
 
 // - Full deck of cards (with suits) (?array)
-    //- shuffle deck
-    //- Deal out 2 cards to each player
+//- shuffle deck
+//- Deal out 2 cards to each player
 //- calculate each players score
 //- declare winner if one has higher score and <= 21
 //- if any player >21 - other player wins
 //- if equal score or both >21 - declare a draw
 
-//hearts, clubs, diamonds, spades
-$cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+function dealCards(array $fullDeck, int $playerCount): array
+{
+    $shuffledFullDeck = $fullDeck;
+    shuffle($shuffledFullDeck);
+    $dealCards = array_rand($shuffledFullDeck, $playerCount * 2);
+    foreach ($dealCards as $card) {
+        $dealtCards[] = $shuffledFullDeck[$card];
+    }
+    return $dealtCards;
+}
+
+
+function createPlayerHand(array $playerCards): array
+{
+    foreach ($playerCards as $playerCard) {
+        $player[] = explode(',', $playerCard);
+    }
+    return $player;
+}
+
+//$cardsPoints = [2=>2, 3=>3, 4=>4, 5=>5, 6=>6, 7=>7, 8=>8, 9=>9, 10=>10, 'J'=>10, 'Q'=>10, 'K'=>10, 'A'=>11];
+$cardTypes = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 $suits = ['hearts', 'clubs', 'diamonds', 'spades'];
 $fullDeck = [];
-
-foreach ($cards as $card) {
-    foreach ($suits as $suit) {
-        $fullDeck[] = $card . ' of ' . $suit;
+foreach ($suits as $suit) {
+    foreach ($cardTypes as $cardType) {
+        $fullDeck[] = $cardType . ',' . $suit;
     }
 }
-echo '<pre>';
-print_r($fullDeck);
-echo '</pre>';
+//echo '<pre>';
+//print_r($fullDeck);
+//echo '</pre>';
 
-//shuffle deck
+$dealtCards = dealCards($fullDeck, 2);
 
-$shuffledFullDeck = $fullDeck;
-shuffle($shuffledFullDeck);
+//echo '<pre>';
+//print_r($dealtCards);
+//echo '</pre>';
 
-echo '<pre>';
-print_r($shuffledFullDeck);
-echo '</pre>';
+$player1Cards = [$dealtCards[0], $dealtCards[1]];
+$player2Cards = [$dealtCards[2], $dealtCards[3]];
 
-//deal cards to players
+//echo '<pre>';
+//print_r($player1Cards);
+//echo '</pre>';
+//
+//echo '<pre>';
+//print_r($player2Cards);
+//echo '</pre>';
 
-$player1Cards = [$shuffledFullDeck[0], $shuffledFullDeck[1]];
-$player2Cards = [$shuffledFullDeck[2], $shuffledFullDeck[3]];
+$player1Hand = createPlayerHand($player1Cards);
 
-echo '<pre>';
-print_r($player1Cards);
-echo '</pre>';
+echo '<p>Player 1 Hand: ' . $player1Hand[0][0] . ' of ' . $player1Hand[0][1] .
+    ' and ' . $player1Hand[1][0] . ' of ' . $player1Hand[1][1] . '</p>';
 
-echo '<pre>';
-print_r($player2Cards);
-echo '</pre>';
+$player2Hand = createPlayerHand($player2Cards);
+
+echo '<p>Player 2 Hand: '. $player2Hand[0][0] . ' of ' . $player2Hand[0][1] .
+    ' and ' . $player2Hand[1][0] . ' of ' . $player2Hand[1][1] . '</p>';
+
+//calculate score
+$player1Score = 0;
+$player2Score = 0;
+
+
+function calcScore(array $playerHand): int
+{
+    $playerScore = 0;
+    foreach ($playerHand as $playerItem) {
+        if (is_numeric($playerItem[0])) {
+            $playerScore += $playerItem[0];
+        } elseif ($playerItem[0] == 'A') {
+            $playerScore += 11;
+        } else {
+            $playerScore += 10;
+        }
+    }
+    return $playerScore;
+}
+
+$player1Score = calcScore($player1Hand);
+
+echo '<p>Player 1 score: ' . $player1Score . '</p>';
+
+$player2Score = calcScore($player2Hand);
+
+echo '<p>Player 2 score: ' . $player2Score . '</p>';
+
+//decide winner/draw
+
+if (($player1Score > 21) && ($player2Score > 21)) {
+    echo '<p>Both players are bust!</p>';
+}
+elseif (($player1Score >21) && !($player2Score > 21)) {
+    echo '<p>Player 1 is bust, Player 2 wins!</p>';
+}
+elseif (($player2Score >21) && !($player1Score > 21)) {
+    echo '<p>Player 2 is bust, Player 1 wins!</p>';
+}
+elseif ($player1Score > $player2Score) {
+    echo '<p>Player 1 wins!</p>';
+}
+elseif ($player2Score > $player1Score) {
+    echo '<p>Player 2 wins!</p>';
+}
+elseif ($player1Score == $player2Score) {
+    echo '<p>It\'s a draw!</p>';
+}
+else {
+    echo '<p>Something has gone wrong...</p>';
+}
